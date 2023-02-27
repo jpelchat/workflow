207bb3dbcb4ddbe8d4746940c03533b447c48e49
Jacob Pelchat <jacob@instruqt.com>
2023-02-27 18:03:18 -0500

    update

--- .github/scripts/production.sh
+++ .github/scripts/production.sh
@@ -4,27 +4,8 @@ set -euxo pipefail
 for dir in tracks/* ; do
     if [ -d "$dir" ]; then
         cd $dir
+        echo $dir IS BEING VALIDATED.
         docker run -e INSTRUQT_TOKEN=$TOKEN --workdir="/track" --mount type=bind,source="$(pwd)",target=/track instruqt/cli track validate
-        cd ..
-    fi
-done
-
-cd $GITHUB_WORKSPACE
-
-for dir in tracks/* ; do
-    if [ -d "$dir" ]; then
-        cd $dir
-        docker run -e INSTRUQT_TOKEN=$TOKEN --workdir="/track" --mount type=bind,source="$(pwd)",target=/track instruqt/cli track push --force
-        cd ..
-    fi
-done
-
-cd $GITHUB_WORKSPACE
-
-for dir in tracks/* ; do
-    if [ -d "$dir" ]; then
-        cd $dir
-        docker run -e INSTRUQT_TOKEN=$TOKEN --workdir="/track" --mount type=bind,source="$(pwd)",target=/track instruqt/cli track test
         cd ..
     fi
 done
