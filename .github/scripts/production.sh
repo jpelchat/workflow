fb748acb0116dff5c4da0f77b9c9e2be675185fa
Jacob Pelchat <jacob@instruqt.com>
2023-02-27 17:50:16 -0500

    update

--- .github/scripts/production.sh
+++ .github/scripts/production.sh
@@ -5,12 +5,14 @@ for dir in tracks/* ; do
     if [ -d "$dir" ]; then
         cd $dir
         docker run -e INSTRUQT_TOKEN=$TOKEN --workdir="/track" --mount type=bind,source="$(pwd)",target=/track instruqt/cli track validate
-        cd ..
     fi
 done
 
+ls
+
 for dir in tracks/* ; do
     if [ -d "$dir" ]; then
+        ls
         cd $dir
         docker run -e INSTRUQT_TOKEN=$TOKEN --workdir="/track" --mount type=bind,source="$(pwd)",target=/track instruqt/cli track push --force
         cd ..
